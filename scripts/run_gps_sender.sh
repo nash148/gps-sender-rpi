c0e7cee6f09783d39ec8575185c9b1af7bbdda77
natan <natansheena@gmail.com>
2022-03-11 12:26:35 +0200

    Removes wating for internet in run_gps_sender.sh

--- scripts/run_gps_sender.sh
+++ scripts/run_gps_sender.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-while ! ping -c 1 -W 1 8.8.8.8; do
-    echo "Waiting for network - network interface might be down..." > /dev/tty1
-    sleep 1
-done
+sleep 5
 
 cd /home/pi/clones/gps-sender-rpi
 sudo python ./src/main.py > /dev/tty1
